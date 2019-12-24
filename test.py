525f9b3d46d5135dbd6c34d7fb2b694a075cb1d8
empramsesii <empramses@outlook.com>
2019-12-24 12:03:47 +0800

    fix typos

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 from asm_exporter import asm_exporter, kill_proc
 code, proc = asm_exporter(
-        proj_pth=~/Code/GhidraProjects/tmp,
-        proj_name=tmp,
-        bin_pth=~/Code/GhidraProjects,
-        bin_name=link_3.elf)
+        proj_pth="~/Code/GhidraProjects/tmp",
+        proj_name="tmp",
+        bin_pth="~/Code/GhidraProjects",
+        bin_name="exercise01.elf")
 # code is a list of ghidra.program.database.code.InstructionDB, proc is the sub-process
 # Use code to do any further analysis
 
+for line in code:
+    print(line.getAddress(), '\t', line)
 
 # Terminate the subprocess with proc
 kill_proc(proc)
