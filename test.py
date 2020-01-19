96251e8bc9875b696dc5352a221ea7747fecdab7
Chunfang Li <empramses@outlook.com>
2020-01-19 17:03:15 +0800

    Change to server version

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ from ghidra_asm_exporter import ghidra_asm_py
 from ghidraType import Program
 
 output_pth = ghidra_asm_py(bin_name="exercise03.elf",
-                           bin_pth="/Users/empramsesii/Code/GhidraProjects/Struct",
-                           output_dir="/Users/empramsesii/Code/GhidraProjects/Struct")
+                           bin_pth="/home/lcf/repos/GhidraProjects/Struct",
+                           output_dir="/home/lcf/repos/GhidraProjects/Struct")
 
 print(output_pth)
 prog = Program(name="test")
